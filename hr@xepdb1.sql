aae5f3e4d4aace5084eb588a07d11902e6d1f759
Roman Shvetsov <shvetsov.roman2022@yandex.ru>
2023-07-15 17:37:19 +0700

    Add inserting into table

--- hr@xepdb1.sql
+++ hr@xepdb1.sql
@@ -7,5 +7,17 @@ from employees;
 select first_name, to_number(to_char(trunc(hire_date, 'yy'), 'yyyy')) as chr
 from employees;
 
-select min(hire_date),max(hire_date),avg(to_number(to_char(hire_date,'yyyy')))
-from employees
+select min(hire_date),max(hire_date),(round( avg(to_number(to_char(hire_date,'yyyy'))),3)) as averege
+from employees;
+
+create table hurricane (name varchar2(64), report_year date, victim number);
+
+select *
+from hurricane;
+
+alter table hurricane modify name not null;
+truncate table hurricane;
+
+insert into hurricane (name,report_year, victim)
+values ('Romchick',sysdate, 1234);
+comment on table hurricane is 'Table for homework';
